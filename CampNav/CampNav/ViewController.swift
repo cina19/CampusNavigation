1923a5f6c08d18ef0b8debcebc187b70cff94ba4
褚娜 <chuna@chunadembp.lan>
2016-05-05 16:03:34 +0800

    校园导航App

---
+++ CampNav/CampNav/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CampNav
+//
+//  Created by 褚娜 on 16/5/5.
+//  Copyright © 2016年 chuna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
